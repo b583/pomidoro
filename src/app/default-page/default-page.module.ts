4c463a1f3cb717025c662d79470ef5391b110604
b583 <bdn583@gmail.com>
2019-05-14 21:01:11 +0200

    Generate timer component

--- src/app/default-page/default-page.module.ts
+++ src/app/default-page/default-page.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DefaultPageComponent } from './default-page/default-page.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
-  declarations: [DefaultPageComponent],
+  declarations: [DefaultPageComponent, TimerComponent],
   imports: [
     CommonModule
   ]
