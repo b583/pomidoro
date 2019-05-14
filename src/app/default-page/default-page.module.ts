6556e273068f4de017b24832a552f4ecac8cba5b
b583 <bdn583@gmail.com>
2019-05-14 22:09:54 +0200

    Implement some of timer components template

--- src/app/default-page/default-page.module.ts
+++ src/app/default-page/default-page.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { DefaultPageComponent } from './default-page/default-page.component';
 import { TimerComponent } from './timer/timer.component';
 
+// Material
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [DefaultPageComponent, TimerComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    // Material
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class DefaultPageModule { }
