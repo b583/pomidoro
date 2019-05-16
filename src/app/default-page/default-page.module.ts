c40556edb6dbe43b1aa4bd25b35f3e80c2510a06
b583 <bdn583@gmail.com>
2019-05-16 17:41:22 +0200

    Add a missing semicolon

--- src/app/default-page/default-page.module.ts
+++ src/app/default-page/default-page.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 import { DefaultPageComponent } from './default-page/default-page.component';
 import { TimerComponent } from './timer/timer.component';
 import { TimersLogComponent } from './timers-log/timers-log.component';
