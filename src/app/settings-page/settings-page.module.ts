245daccd4a574806a8ab730b900268c6ee69414d
b583 <bdn583@gmail.com>
2019-05-18 13:02:15 +0200

    Implement simple SettingsPage concept

--- src/app/settings-page/settings-page.module.ts
+++ src/app/settings-page/settings-page.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsPageComponent } from './settings-page.component';
 
+//Material
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 @NgModule({
   declarations: [SettingsPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    // Material
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class SettingsPageModule { }
